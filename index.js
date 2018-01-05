dc5e6db84313c3b0e36044e85445500ba92c7d84
Simon Massey <simon@simonmassey.org>
2018-01-05 08:18:07 +0000

    loads a list of templates

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
-// dependency inject config so that evn var overrides can be tested in the real app use `var config = require('nconf');`
+
+/**
+ * Parameters are loaded by argv, then env, then parameters.yaml. This allows overrides to be supplied by argv and env. It also ensures that if a developer commits a platform property to the parameters file that is supplied by env then it will be safely ignored. This means that the platform can impose the SDK version. 
+ * 
+ * @param {*} nconf dependency inject config so that evn var overrides can be tested in the real app use `var config = require('nconf');`
+ * @param {*} folder the folder contain parameters.yaml defaults to '.'
+ */
 function parameters(nconf, folder = '.') {
     nconf.argv()
       .env()
@@ -7,6 +13,9 @@ function parameters(nconf, folder = '.') {
   return nconf;
 }
 
-const templates = ['image-streams.json', 'build-config.yaml']
+function templates(parameters) {
+  return parameters.get('templates')
+}
 
-module.exports['parameters'] = parameters;
+module.exports['parameters'] = parameters
+module.exports['templates'] = templates
--- test/testrunner.js
+++ test/testrunner.js
@@ -2,30 +2,47 @@ const test = require('unit.js')
 const assert = require('assert')
 
 // basically when you require('config') is initializes from env vars so we use 'import-fresh' to reload it in different tests 
-const importFresh = require("import-fresh")
+const importFresh = require('import-fresh')
 
 // code under test
 const parameters = require('../index.js').parameters
 
+// code under test
+const templates = require('../index.js').templates
+
 describe('Learning by the example', function(){
  
   it('loads dotnetVersion from local test/parameters.yaml as 2.0-9', function(){
-    const nconf = importFresh("nconf")
-    const dotnetVersion = parameters(nconf, 'test').get("dotnetVersion")
+    const nconf = importFresh('nconf')
+    const dotnetVersion = parameters(nconf, 'test').get('dotnetVersion')
     test.string(dotnetVersion).startsWith('2.0-9')
   });
 
   it('env var can overload dotnetVersion in parameters.yaml as 2.0-10', function(){
-    process.env['dotnetVersion'] = "2.0-10"
-    const nconf = importFresh("nconf")
-    const dotnetVersion = parameters(nconf, 'test').get("dotnetVersion")
+    process.env['dotnetVersion'] = '2.0-10'
+    const nconf = importFresh('nconf')
+    const dotnetVersion = parameters(nconf, 'test').get('dotnetVersion')
     test.string(dotnetVersion).startsWith('2.0-10')
   });
 
   it('loads rshinyVersion from local test/parameters.yaml as 2.0-9', function(){
-    const nconf = importFresh("nconf")
-    const dotnetVersion = parameters(nconf, 'test').get("rshinyVersion")
-    test.string(dotnetVersion).startsWith('1.5.3.838')
+    const nconf = importFresh('nconf')
+    const whatever = parameters(nconf, 'test').get('rshinyVersion')
+    test.string(whatever).startsWith('1.5.3.838')
+  });
+
+  it('loads dotnetVersions from local test/parameters.yaml', function(){
+    const nconf = importFresh('nconf')
+    const dotnetVersions = parameters(nconf, 'test').get('dotnetVersions')
+    test.object(dotnetVersions).contains(['2.0-9','2.0-10']);
   });
  
+  it('loads templates from local test/parameters.yaml', function(){
+    const nconf = importFresh('nconf')
+    const ts = templates(parameters(nconf, 'test'))
+    test.object(ts).contains(['image-streams.json', 'build-config.yaml'])
+  });
+  
+    
+
 });
