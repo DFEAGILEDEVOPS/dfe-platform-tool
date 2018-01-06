ac0a62c3faa9e83b7ea6dbe23be7822e015db096
Simon Massey <simon@simonmassey.org>
2018-01-06 21:12:14 +0000

    properties file is explicitly named

--- dfe.js
+++ dfe.js
@@ -1,30 +1,19 @@
-
 /**
  * Parameters are loaded by argv, then env, then parameters.yaml. This allows overrides to be supplied by argv and env. It also ensures that if a developer commits a platform property to the parameters file that is supplied by env then it will be safely ignored. This means that the platform can impose the SDK version. 
  * 
  * @param {*} nconf dependency inject config so that evn var overrides can be tested in the real app use `const config = require('nconf')`
- * @param string folder contain parameters.yaml. defaults to '.'
+ * @param string propertiesFilePath contain parameters.yaml. defaults to '.'
  */
-function parameters(nconf, folder = '.') {
+function parameters(nconf, propertiesFilePath) {
     nconf.argv()
       .env()
-      .file({ file: folder + '/parameters.yaml', format: require('nconf-yaml') });
+      .file({ file: propertiesFilePath, format: require('nconf-yaml') });
   return nconf;
 }
 
-// const fs = require('fs')
-// const _ = require('lodash')
+function templateParametersValues(templateParameters) {
 
-// function loadTemplates(parameters) {
-//   _.forEach(parameters.get('templates'), function(tmplt){
-//     const content = fs.readFileSync(tmplt)
-//     if( tmplt.endsWith('json')){
-//       console.log('tmplt ends with json')
-//     } else if( tmplt.endsWith('yaml')){
-//       console.log('tmplt ends with yaml')
-//     } else throw 'unrecognised file extension on file: '+tmplt
-//   } );
-// }
+}
 
 module.exports['parameters'] = parameters
 
--- test/testrunner.js
+++ test/testrunner.js
@@ -11,42 +11,48 @@ describe('dfe platform tooling', function(){
  
   it('loads dotnetVersion from local test/parameters.yaml as 2.0-9', function(){
     const nconf = importFresh('nconf')
-    const dotnetVersion = parameters(nconf, 'test').get('dotnetVersion')
+    const dotnetVersion = parameters(nconf, 'test/myapp-properties.yaml').get('dotnetVersion')
     test.string(dotnetVersion).startsWith('2.0-9')
   });
 
   it('env var can overload dotnetVersion in parameters.yaml as 2.0-10', function(){
     process.env['dotnetVersion'] = '2.0-10'
     const nconf = importFresh('nconf')
-    const dotnetVersion = parameters(nconf, 'test').get('dotnetVersion')
+    const dotnetVersion = parameters(nconf, 'test/myapp-properties.yaml').get('dotnetVersion')
     test.string(dotnetVersion).startsWith('2.0-10')
   });
 
   it('loads rshinyVersion from local test/parameters.yaml as 2.0-9', function(){
     const nconf = importFresh('nconf')
-    const whatever = parameters(nconf, 'test').get('rshinyVersion')
+    const whatever = parameters(nconf, 'test/myapp-properties.yaml').get('rshinyVersion')
     test.string(whatever).startsWith('1.5.3.838')
   });
 
   it('loads dotnetVersions from local test/parameters.yaml', function(){
     const nconf = importFresh('nconf')
-    const dotnetVersions = parameters(nconf, 'test').get('dotnetVersions')
+    const dotnetVersions = parameters(nconf, 'test/myapp-properties.yaml').get('dotnetVersions')
     test.object(dotnetVersions).contains(['2.0-9','2.0-10']);
   });
  
   it('loads template parameters from local test/parameters.yaml', function(){
     const nconf = importFresh('nconf')
-    const ts = parameters(nconf, 'test').get('parameters')
-    const params = ts['image-streams.json']
+    const templateParameters = parameters(nconf, 'test/myapp-properties.yaml').get('templateParameters')
+    const params = templateParameters['dotnet-example.json']
     test.object(params).contains({DOTNET_IMAGE_STREAM_TAG: { desc: 'The image stream tag which is used to build the code.',
       value: 'dotnet:2.0' }})
   });
 
   it('handles empty parameters from local test/parameters.yaml', function(){
     const nconf = importFresh('nconf')
-    const ts = parameters(nconf, 'test').get('parameters')
-    const params = ts['image-streams.json']
+    const templateParameters = parameters(nconf, 'test/myapp-properties.yaml').get('templateParameters')
+    const params = templateParameters['dotnet-example.json']
     test.object(params).contains({CONTEXT_DIR: { desc: 'Set this to use a subdirectory of the source code repository', value: null}})
   });
   
+  it('can convert non-emptry parameters from local test/parameters.yaml as tuples of name and value', function(){
+    const nconf = importFresh('nconf')
+    const templateParameters = parameters(nconf, 'test/myapp-properties.yaml').get('templateParameters')
+    const params = templateParameters['dotnet-example.json']
+    test.object(params).contains({CONTEXT_DIR: { desc: 'Set this to use a subdirectory of the source code repository', value: null}})
+  });
 });
