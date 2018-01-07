7bc66f62c1f122394ad86670b1ac1f0f67e4ef17
Simon Massey <simon@simonmassey.org>
2018-01-07 20:26:13 +0000

    switched to a switch statement

--- index.js
+++ index.js
@@ -6,32 +6,34 @@ const nconf = require('nconf')
 // a command line parse. when running "npm run_script" the first two process.argv are "../bin/node" and "index.js" which we drop
 const argv = require('minimist')(process.argv.slice(2,process.argv.length))
 
-// command line args that don't hace a -x or --x are collected into an array called '_' so we inspect at the first one
+// command line args that down start with '_' are collected into an array called '_' so we inspect at the first one
 const command = argv._[0]
 
-if( command == "parameters") {
-    const template = argv.t
-    const properties = argv.c
-    const templateParameters = dfe.templateParameters
-    const parameters = templateParameters(nconf, properties, template)
+switch(command) {
+    case "parameters":
+        const template = argv.t
+        const properties = argv.c
+        const templateParameters = dfe.templateParameters
+        const parameters = templateParameters(nconf, properties, template)
 
-    for( const key in parameters ) {
-        console.log(key+"="+parameters[key])
-    }
-
-} else if ( command == "scmsecret" ) {
-    // https://blog.openshift.com/using-ssh-key-for-s2i-builds/
-    const sshprivatekey = argv._[1]
-    if( sshprivatekey.startsWith('ssh-privatekey=') ) {
-        console.log("oc secrets new scmsecret "+sshprivatekey)
-        console.log("oc secrets link builder scmsecret")
-    } else {
-        const msg = "ERROR the second paramaeter should start ssh-privatekey="
-        console.error(msg)
-        throw new Error("Invalid arguments. "+msg)
-    }
-} else {
-     throw new Error("Unrecognised command: "+command);
+        for( const key in parameters ) {
+            console.log(key+"="+parameters[key])
+        }
+        break;
+    case "scmsecret":
+        // https://blog.openshift.com/using-ssh-key-for-s2i-builds/gi
+        const sshprivatekey = argv._[1]
+        if( sshprivatekey.startsWith('ssh-privatekey=') ) {
+            console.log("oc secrets new scmsecret "+sshprivatekey)
+            console.log("oc secrets link builder scmsecret")
+        } else {
+            const msg = "ERROR the second paramaeter should start ssh-privatekey="
+            console.error(msg)
+            throw new Error("Invalid arguments. "+msg)
+        }
+        break;
+    default:
+        throw new Error("Unrecognised command: "+command);
 }
 
     
