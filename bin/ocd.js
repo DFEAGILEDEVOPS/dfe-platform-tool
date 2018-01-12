97a97f55c8b2b41a975539bb8972556c8dde4a7d
Simon Massey <simon@simonmassey.org>
2018-01-12 23:28:48 +0000

    git secret testing

--- bin/ocd.js
+++ bin/ocd.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 // the main library of this tool
-const dfe = require('../ocd-lib.js')
+const ocd = require('../ocd-lib.js')
 // a config library that does overrides  
 const nconf = require('nconf')
 // a command line parse. when running "npm run_script" the first two process.argv are "../bin/node" and "index.js" which we drop
@@ -17,7 +17,7 @@ switch(command) {
     case "parameters":
         const template = argv.t
         const properties = argv.c
-        const templateParameters = dfe.templateParameters
+        const templateParameters = ocd.templateParameters
         const parameters = templateParameters(nconf, properties, template)
 
         for( const key in parameters ) {
